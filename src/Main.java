97d7c5442e4d29c90162ea67ae065a4485008437
Daniel Tar <tardani95@gmail.com>
2018-04-17 23:23:21 +0200

    file read tests - git line separator setting changed to crlf

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Graph myproblem = new Graph();
 
+        File sText = new File("Stext.txt");
+        File wText = new File("Wtext.txt");
+        Scanner inputS = new Scanner(sText);
+        Scanner inputW = new Scanner(wText);
 
+        int count = 0;
+        int ptpcount = 0;
 
-        System.out.println("Hello World!");
+        while (inputW.hasNext()) {
+            // is peer to peer connection between the cars
+            if (inputW.nextInt() == 1) {
+                //System.out.print("1");
+                ptpcount++;
+            }
+            count++;
+        }
+        System.out.println("");
+        System.out.println(ptpcount+" ptp connections");
+        System.out.println(count + " integers read");
     }
 }
