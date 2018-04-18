dfe63b4a12522f1988ced2c8a4d6693417234556
Daniel Tar <tardani95@gmail.com>
2018-04-18 15:39:23 +0200

    java program for transforming input file to matlab functions

---
+++ room2/task11/input_to_matlab_format/src/com/nanproduction/Main.java
@@ -0,0 +1,38 @@
+package com.nanproduction;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        // write your code here
+        File inputFile = new File("input.txt");
+        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+
+        Scanner input = new Scanner(inputFile);
+        String line = "";
+        String[] array = null;
+        while (input.hasNext()) {
+            line = input.nextLine();
+            array = line.split(" ");
+            if (line.contains("oae")) {
+                //G = oae(G, v1,v2);
+                writer.println("G = oae(G," + array[0] + "," + array[2] + ");");
+            }
+            if (line.contains("oat")) {
+                writer.println("G = oat(G," + array[0] + "," + array[2] + ");");
+            }
+            if (line.contains("kem")) {
+                writer.println("G = kem(G," + array[0] + "," + array[2] + ");");
+            }
+            if (line.contains("eet")) {
+                writer.println("G = eet(G," + array[0] + "," + array[1] + ");");
+            }
+        }
+        writer.close();
+    }
+}
